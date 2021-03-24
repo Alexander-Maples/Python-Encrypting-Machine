a6fb94e9dce6f94b19210d9ac94b1b7947cb2ab9
Alexander Maples <mapleale@studentmail.conroeisd.net>
2021-03-24 19:23:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import socket
+'''import wordart
+import random
+import time
+import sys'''
+def clear():
+				os.system("clear")
+clear()
+In = ""
+Out = ""
+clear()
+print(os.name)
+print(sys.platform)
+print(socket.gethostname())
+print(socket.getfqdn())
+In = input('What is your text that needs to be encrypted?\n')
+Out = In
+clear()
+sys.exit("This is not finished!")
+
+
+
+
+
+
+
+
+
+print('Hi, %s.' % Out)
+sys.exit("This is not finished!")
